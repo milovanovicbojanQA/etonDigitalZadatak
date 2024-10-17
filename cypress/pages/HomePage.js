60a6e0fdc144d73e2bc15193674b02aba16c4fa0
milovanovicbojanQA <bojanmilovanovic1@icloud.com>
2024-10-17 21:09:49 +0200

    Zadatak

--- cypress/pages/HomePage.js
+++ cypress/pages/HomePage.js
@@ -1,52 +1,45 @@
 import Selectors from './Selectors';
 
 export default class HomePage {
-  static visit() {
-    cy.visit('https://www.holycode.com/'); // Poseti Holycode stranicu
-  }
+    static visit() {
+        cy.visit('https://www.holycode.com/'); // Poseti Holycode stranicu
+    }
 
-//   static clickCareersDropdown() {
-//     //this.handleCookies(); // Pobrini se da se kolačići prihvate
-    
-//     // Hover na Career meni
-//     cy.get(Selectors.careersDropdown)
-//       .should('be.visible')
-//       //.trigger('mouseover'); // Simuliraj hover
-//       .click();
-//       cy.wait(2000);
-  
-//     // Sačekaj da se "Open Positions" učita i klikni na njega
-//     cy.get(Selectors.careersLink)
-//       .should('be.visible')
-//       .click(); // Klikni na Open Positions
-//   }
+    static openPositions() {
+        // Scroll do dugmeta i klik na njega
+        return cy.get(Selectors.openPositionsButton)
+            .scrollIntoView()
+            .should('be.visible')
+            .click() // Klikni na "Open Positions"
+            .catch((error) => {
+                cy.log('An error occurred while opening positions:', error); // Zapiši grešku
+            });
+    }
 
-static openPositions() {
-    // Scroll do dugmeta i klik na njega
-    cy.get(Selectors.openPositionsButton)
-      .scrollIntoView()
-      .should('be.visible')
-      .click(); // Klikni na "Open Positions"
+    static handleCookies() {
+        // Klik na "Allow all" ako je dugme vidljivo
+        cy.get(Selectors.acceptCookiesButton, { timeout: 10000 }).then(($btn) => {
+            if ($btn.is(':visible')) {
+                cy.wrap($btn).click(); // Klikni na dugme
 
-    // Proveri da je otvoren odgovarajući URL
-    //cy.url().should('include', '/careers'); // ili tačan URL sa eq
-  }
-  
-  static handleCookies() {
-    // Klik na "Allow all" ako je dugme vidljivo
-    cy.get(Selectors.acceptCookiesButton, { timeout: 10000 }).then(($btn) => {
-      if ($btn.is(':visible')) {
-        cy.wrap($btn).click(); // Klikni na dugme
+                // Proveri da popup više ne postoji pre nastavka
+                // cy.get(Selectors.acceptCookiesButton, { timeout: 5000 })
+                // .should('not.exist');
+                // Ostavljeno kao opcija
+            }
+        });
+    }
 
-        // // Proveri da popup više ne postoji pre nastavka
-        // cy.get(Selectors.acceptCookiesButton, { timeout: 5000 })
-        //   .should('not.exist');
-      }
-    });
-  }
-  
+    static checkPageTitle() {
+        cy.get(Selectors.pageTitle, { timeout: 3000 }) // Povećan timeout za mrežna kašnjenja
+            .should('be.visible') // Proveri da je naslov vidljiv
+            .then(($title) => {
+                // Proveri tekst naslova
+                expect($title).to.have.text('Tech Solutions to scale');
+            })
+            .catch((error) => {
+                cy.log('Error checking page title:', error.message); // Loguj grešku ako dođe do problema
+            });
+    }
 
-  static checkPageTitle() {
-    cy.get(Selectors.pageTitle).should('have.text', 'Tech Solutions to scale');
-  }
 }
